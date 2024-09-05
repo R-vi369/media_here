e2dcd9082c9e015d6cdfd919bb2d0860f569a164
ravi <sama369.rk@gmail.com>
2024-09-05 18:26:44 +0530

    MEDIA

---
+++ src/About.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <>
+
+
+            <div class="flex  px-6 md:px-20  items-center justify-center bg-hero md:h-[90vh] overflow-hidden">
+                <div class="flex flex-col  gap-6 md:flex-row items-center max-w-8xl">
+
+                    <div class="w-full md:w-1/2 flex justify-center md:justify-end">
+                        <img src="https://emeritus.org/in/wp-content/uploads/sites/3/2022/11/IIMK-ASMP.jpg.optimal.jpg" />
+                    </div>
+                    <div class="w-full md:w-1/2 lg:pr-32">
+                    <h1 className='pb-2 text-3xl font-bold text-blue-900'>About us</h1>
+                        <h2 class="text-4xl lg:text-5xl text-center md:text-left text-blue-500 leading-tight font-medium">
+                            Dedicated to work for the better future
+                        </h2>
+                        <h3
+                            class="mt-6 md:mt-10 text-md lg:text-xl text-center md:text-left text-gray-700 font-light tracking-wider leading-relaxed">
+                            hello there we are WE-DIGI, we are here to help all the people who wants be online or make their business online.
+                        </h3>
+                        <div class="mt-10 flex flex-col sm:flex-row justify-center md:justify-start">
+                            <button class="w-full sm:w-40 px-4 py-3 rounded font-semibold text-md bg-blue-500 text-white border-2 border-blue-500">Get started</button>
+                            <button class="w-full mt-4 sm:mt-0 sm:ml-4 sm:w-40 px-4 py-3 rounded font-semibold text-md bg-white text-blue-500 border-2 border-gray-500">Watch a Demo</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+        </>
+    )
+}
+
+export default About
---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Footer from "./Footer"
+import Hero from "./Hero"
+import Navbar from "./Navbar"
+import Testimonial from "./Testimonial"
+import About from './About'
+import Service from "./Service"
+
+const App = () => {
+  return (
+    <>
+
+      <Navbar />
+      <Hero />
+      <About />
+      <Service />
+      <Testimonial />
+      <Footer />
+
+
+    </>
+  )
+}
+
+export default App
